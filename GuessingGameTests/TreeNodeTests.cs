e3983e3f9bc1a3db84f67cf9cf1e63e249007e8e
Fellipe Lamoglia <Fellipe Lamoglia>
2016-04-04 14:41:47 -0300

    Creates unit test project and initial tests for TreeNode class

---
+++ GuessingGameTests/TreeNodeTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GuessingGame.data;
+
+namespace GuessingGameTests
+{
+    [TestClass]
+    public class TreeNodeTests
+    {
+        private const string ANIMAL_DOG = "Dog";
+        private const string ANIMAL_PLATYPUS = "Platypus";
+        private const string ANIMAL_EAGLE = "Eagle";
+
+        private const string TRAIT_DOG = "barks";
+        private const string TRAIT_PLATYPUS = "lays eggs";
+        private const string TRAIT_EAGLE = "flies";
+
+        [TestMethod]
+        public void NonLeafNode_ShouldHaveTwoLeafNodes()
+        {
+            TreeNode node = new TreeNode(TRAIT_DOG, ANIMAL_DOG, ANIMAL_PLATYPUS);
+
+            //node is a non leaf node
+            Assert.IsFalse(node.isLeafNode());
+
+            //and has two leaf nodes
+            Assert.IsTrue(node.getYesNode().isLeafNode());
+            Assert.IsTrue(node.getNoNode().isLeafNode());
+        }
+
+        [TestMethod]
+        public void UpdateLeafNode_ShoulCreateTwoLeafNodes_WithCorrectContents()
+        {
+            TreeNode node = new TreeNode(TRAIT_DOG, ANIMAL_DOG, ANIMAL_PLATYPUS);
+            TreeNode leafNode = node.getYesNode();
+
+            //leafNode is a leaf node
+            Assert.IsTrue(leafNode.isLeafNode());
+            String previouslyStoredData = leafNode.getData();
+
+            leafNode.updateLeafNode(TRAIT_EAGLE, ANIMAL_EAGLE);
+
+            //leafNode is no more a leaf node
+            Assert.IsFalse(leafNode.isLeafNode());
+
+            //and now shas two leaf nodes
+            Assert.IsTrue(leafNode.getYesNode().isLeafNode());
+            Assert.IsTrue(leafNode.getNoNode().isLeafNode());
+
+            //its content is the new animal trait
+            Assert.AreSame(leafNode.getData(), TRAIT_EAGLE);
+            //its "yes" node contains the new animal
+            Assert.AreSame(leafNode.getYesNode().getData(), ANIMAL_EAGLE);
+            //its "no" node contains the previously stored data
+            Assert.AreSame(leafNode.getNoNode().getData(), previouslyStoredData);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void UpdateNonLeafNode_ShouldThrowException()
+        {
+            TreeNode node = new TreeNode(TRAIT_DOG, ANIMAL_DOG, ANIMAL_PLATYPUS);
+
+            //node is a non leaf node
+            Assert.IsFalse(node.isLeafNode());
+            node.updateLeafNode(TRAIT_PLATYPUS, ANIMAL_PLATYPUS);
+        }
+    }
+}
